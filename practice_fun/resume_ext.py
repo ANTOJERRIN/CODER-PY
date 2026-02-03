47a7995c40a0865964a550728874e94177d9ed68
ANTOJERRIN <jerrinanto0@gmail.com>
2026-02-03 23:14:31 +0530

    resumecode is being coded

---
+++ practice_fun/resume_ext.py
@@ -0,0 +1,13 @@
+def reu():
+    name=input()
+    age=int(input())
+    exp=float(input())
+    knpy=True
+    skills=input().strip()
+    print("Name: ",name,"(str)")
+    print("Age: ",age,"(int)")
+    print("Experience: ",exp,"(float)")
+    print("Knows Python: ",knpy,"(bool)")
+    print("Skills: [",skills,"] (list)")
+reu()    
+
